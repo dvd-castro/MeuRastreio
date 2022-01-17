c87cfc6f024ac9994095c5e964e85d5a028958b7
David Castro <david.dev.designer@gmail.com>
2022-01-17 17:09:22 -0300

    Adicionando métodos base

--- app/src/main/java/br/com/davidcastro/meurastreio/viewModel/MainViewModel.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/viewModel/MainViewModel.kt
@@ -3,11 +3,64 @@ package br.com.davidcastro.meurastreio.viewModel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.davidcastro.meurastreio.data.model.RastreioModel
 import br.com.davidcastro.meurastreio.data.repository.RastreioRepository
 import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val repository: RastreioRepository
 ): ViewModel() {
+
+    fun getAllTracking() = viewModelScope.launch {
+        try {
+            val all = repository.getAllTracking()
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { localizedMessage ->
+                Log.e("ERROR", localizedMessage)
+            }
+        }
+    }
+
+    //Pega um rastreio armazenado localmente se ele existir
+    fun getTracking(codigo: String) = viewModelScope.launch {
+        try {
+            val tracking = repository.getTracking(codigo = codigo)
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { localizedMessage ->
+                Log.e("ERROR", localizedMessage)
+            }
+        }
+    }
+
+    //Procura um rastreio na api
+    fun findTracking(codigo: String) = viewModelScope.launch {
+        try {
+            val tracking = repository.findTracking(codigo = codigo)
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { localizedMessage ->
+                Log.e("ERROR", localizedMessage)
+            }
+        }
+    }
+
+    fun deleteTracking(codigo: String) = viewModelScope.launch {
+        try {
+            repository.deleteTracking(codigo = codigo)
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { localizedMessage ->
+                Log.e("ERROR", localizedMessage)
+            }
+        }
+    }
+
+    fun insertTracking(rastreio: RastreioModel) = viewModelScope.launch {
+        try {
+            repository.insertTracking(rastreio = rastreio)
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { localizedMessage ->
+                Log.e("ERROR", localizedMessage)
+            }
+        }
+    }
 
 }
