3f4e271f32060f8c245c22615b451493fcc559e8
David Castro <david.dev.designer@gmail.com>
2022-02-10 21:38:27 -0300

    Começando a adicionar método para verificar atualizações das encomendas

--- app/src/main/java/br/com/davidcastro/meurastreio/helpers/utils/AlarmReceiver.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/helpers/utils/AlarmReceiver.kt
@@ -4,15 +4,47 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
+import br.com.davidcastro.meurastreio.R
+import br.com.davidcastro.meurastreio.data.repository.RastreioRepository
+import kotlinx.coroutines.*
+import org.koin.java.KoinJavaComponent.inject
 
 class AlarmReceiver : BroadcastReceiver() {
 
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    private val repository: RastreioRepository by inject(RastreioRepository::class.java)
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == "android.intent.action.BOOT_COMPLETED") {
-            Log.d("####","iniciou")
+            scope.launch {
+                checkIfHaveUpdates(context)
+            }
         }
-        Log.d("####","iniciou 2")
-        Toast.makeText(context,"DSadasdsadsa",Toast.LENGTH_LONG).show()
+        scope.launch {
+            checkIfHaveUpdates(context)
+        }
+    }
+
+    private suspend fun checkIfHaveUpdates(context: Context) {
+
+        try {
+            val all = repository.getAllTracking()
+            if(all.isNotEmpty()){
+                all.forEach { rastreio ->
+                    if(rastreio.eventos.first().status != context.getString(R.string.status_entregue)) {
+                        val rastreioVerificado = repository.findTracking(rastreio.codigo)
+                        if(rastreio.eventos.first() != rastreioVerificado.eventos.first()) {
+                            //TODO Notificar
+                        }
+                    }
+                }
+            }
+
+        } catch (ex: Exception) {
+            ex.localizedMessage?.let { localizedMessage ->
+                Log.e("ERROR -> Verify If Tracking Exists", localizedMessage)
+            }
+        }
     }
 }
