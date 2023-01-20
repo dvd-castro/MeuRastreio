399125028373f2dbebaa35efd9e66fddfb0b44c6
David Castro <51448763+dvd-castro@users.noreply.github.com>
2023-01-20 12:02:05 -0300

    Adicionando testes unitários

---
+++ tests/src/test/java/br/com/davidcastro/meurastreio/repositories/TrackingRepositoryImplTest.kt
@@ -0,0 +1,33 @@
+package br.com.davidcastro.meurastreio.repositories
+
+import br.com.davidcastro.data.api.TrackingApi
+import br.com.davidcastro.data.model.TrackingModel
+import br.com.davidcastro.data.repository.TrackingRepositoryImpl
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.Response
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class TrackingRepositoryImplTest {
+
+    private val trackingApi: TrackingApi = mockk()
+    private val repository: TrackingRepositoryImpl = TrackingRepositoryImpl(trackingApi)
+    private val response = Response.success(mockk<TrackingModel>())
+
+    @Test
+    fun `when get tracking with success`() = runTest {
+        val expectedResult = response
+
+        coEvery {
+            trackingApi.getTracking(any(), any(), any())
+        } returns response
+
+        val result = repository.getTracking("")
+
+        Assert.assertEquals(expectedResult, result)
+    }
+}
