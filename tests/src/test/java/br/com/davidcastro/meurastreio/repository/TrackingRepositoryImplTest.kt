22e008e8bfe1a442ebd586f8f98478291300f776
David Castro <david.dev.designer@gmail.com>
2023-04-03 20:05:39 -0300

    TrackingRepositoryImplTest.kt

--- tests/src/test/java/br/com/davidcastro/meurastreio/repository/TrackingRepositoryImplTest.kt
+++ tests/src/test/java/br/com/davidcastro/meurastreio/repository/TrackingRepositoryImplTest.kt
@@ -1,34 +1,61 @@
 package br.com.davidcastro.meurastreio.repository
 
+import TrackingResponseRealMock
 import br.com.davidcastro.data.api.TrackingApi
 import br.com.davidcastro.data.model.TrackingModel
+import br.com.davidcastro.data.repository.TrackingRepository
 import br.com.davidcastro.data.repository.TrackingRepositoryImpl
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
 import retrofit2.Response
-import kotlin.test.assertFailsWith
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class TrackingRepositoryImplTest {
 
     private val trackingApi: TrackingApi = mockk()
-    private val repository: TrackingRepositoryImpl = TrackingRepositoryImpl(trackingApi)
-    private val response: Response<TrackingModel> = mockk()
+    private val repository: TrackingRepository = TrackingRepositoryImpl(trackingApi)
 
     @Test
     fun `when get tracking has success`() = runTest {
-        val expectedResult = response
+
+        val expectedResult = Response.success(TrackingResponseRealMock.getTrackingModelResponse())
 
         coEvery {
             trackingApi.getTracking(any(), any(), any())
-        } returns response
+        } returns expectedResult
 
-        val result =   repository.getTracking("")
+        val result = repository.getTracking(anyString())
 
         Assert.assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `when get tracking has error 500 with body null`() = runTest {
+
+        val expectedResult = Response.error<TrackingModel>(500, ResponseBody.create(null, ""))
+
+        coEvery {
+            trackingApi.getTracking(any(), any(), any())
+        } returns expectedResult
+
+        val result = repository.getTracking(anyString())
+
+        Assert.assertEquals(expectedResult, result)
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun `when get tracking has exception`() = runTest {
+
+        coEvery {
+            trackingApi.getTracking(any(), any(), any())
+        }.throws(RuntimeException())
+
+        repository.getTracking(anyString())
     }
 }
