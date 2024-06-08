2b00642c36978b3bc5db4f36da3575945231e4f0
David Castro <david.dev.designer@gmail.com>
2024-06-08 11:54:21 -0300

    Ajustando tamanho do loading
    Signed-off-by: David Castro <david.dev.designer@gmail.com>

--- app/src/main/java/br/com/davidcastro/meurastreio/features/home/view/components/Loading.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/features/home/view/components/Loading.kt
@@ -3,6 +3,7 @@ package br.com.davidcastro.meurastreio.features.home.view.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -10,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.davidcastro.meurastreio.R
 import br.com.davidcastro.meurastreio.core.theme.GetPrimaryColor
+import br.com.davidcastro.meurastreio.core.utils.Dimens
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
@@ -25,7 +27,8 @@ fun Loading(
             modifier = Modifier.background(GetPrimaryColor()).fillMaxSize()
         ) {
             LottieAnimation(
-                composition = composition
+                composition = composition,
+                modifier = Modifier.size(Dimens.dimen200dp)
             )
         }
     }
