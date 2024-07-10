7519f8e69373739a0f352c90fc4288ddc10ce070
David Castro <david.dev.designer@gmail.com>
2024-07-10 12:02:37 -0300

    Adicionando card de atualizado
    Signed-off-by: David Castro <david.dev.designer@gmail.com>

--- app/src/main/java/br/com/davidcastro/meurastreio/commons/components/TrackingCard.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/commons/components/TrackingCard.kt
@@ -1,20 +1,28 @@
 package br.com.davidcastro.meurastreio.commons.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.davidcastro.meurastreio.commons.utils.Dimens
 import br.com.davidcastro.meurastreio.commons.utils.Utils.getTrackingStatusColor
 import br.com.davidcastro.meurastreio.core.theme.GetCardBackgroundColor
+import br.com.davidcastro.meurastreio.core.theme.GetPrimaryColor
+import br.com.davidcastro.meurastreio.core.theme.Red
 
 
 @Composable
@@ -25,6 +33,7 @@ fun TrackingCard(
     status: String,
     local: List<String>,
     date: String,
+    hasUpdate: Boolean = false,
     onItemClick: () -> Unit = {}
 ) {
     Card(
@@ -44,11 +53,27 @@ fun TrackingCard(
         ) {
 
             name?.let {
-                Text(
-                    text = it,
-                    fontSize = Dimens.size16sp,
-                    fontWeight = FontWeight.Medium
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = it,
+                        fontSize = Dimens.size16sp,
+                        fontWeight = FontWeight.Medium,
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    if(hasUpdate) {
+                        Text(
+                            modifier = Modifier
+                                .clip(AbsoluteRoundedCornerShape(Dimens.dimen8dp))
+                                .background(Red)
+                                .padding(Dimens.dimen4dp),
+                            text = "Atualizado",
+                            color = GetPrimaryColor()
+                        )
+                    }
+                }
             }
 
             code?.let {
@@ -89,4 +114,16 @@ fun TrackingCard(
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun TrackingCardPreview() {
+    TrackingCard(
+        name = "David",
+        status = "Objeto entregue ao destinatário",
+        local = listOf("São Paulo"),
+        date = "26/11/1999",
+        hasUpdate = true
+    )
 }
