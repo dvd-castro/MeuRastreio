a10888c7ca8f50431da61a9cfc7bef50442918be
David Castro <david.dev.designer@gmail.com>
2024-07-10 10:48:36 -0300

    Removendo atributo não utilizado
    Signed-off-by: David Castro <david.dev.designer@gmail.com>

--- app/src/main/java/br/com/davidcastro/meurastreio/features/details/mvi/DetailsState.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/features/details/mvi/DetailsState.kt
@@ -1,6 +1,5 @@
 package br.com.davidcastro.meurastreio.features.details.mvi
 
 data class DetailsState(
-    val operationCompleted: Boolean = false,
-    val showSetNameDialog: Boolean = false,
+    val showSetNameDialog: Boolean = false
 )
