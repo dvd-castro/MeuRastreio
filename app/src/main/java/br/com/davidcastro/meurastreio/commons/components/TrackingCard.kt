07883eae805cf0df06f6b407ed41424494dfe051
David Castro <david.dev.designer@gmail.com>
2024-08-28 18:23:52 -0300

    Capitalizando nome do rastreio
    Signed-off-by: David Castro <david.dev.designer@gmail.com>

--- app/src/main/java/br/com/davidcastro/meurastreio/commons/components/TrackingCard.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/commons/components/TrackingCard.kt
@@ -17,7 +17,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.davidcastro.meurastreio.R
 import br.com.davidcastro.meurastreio.commons.utils.Dimens
@@ -58,7 +60,7 @@ fun TrackingCard(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
-                        text = it,
+                        text = it.capitalize(Locale.current),
                         fontSize = Dimens.size16sp,
                         fontWeight = FontWeight.Medium,
                         modifier = Modifier.weight(1f)
