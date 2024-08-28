0e1c69b1e931d40a1851ca0e425ee453080cc464
David Castro <david.dev.designer@gmail.com>
2024-08-28 18:27:58 -0300

    Mostrando mensagem de lista vazia
    Signed-off-by: David Castro <david.dev.designer@gmail.com>

--- app/src/main/java/br/com/davidcastro/meurastreio/features/home/view/components/HomeTrackingCardList.kt
+++ app/src/main/java/br/com/davidcastro/meurastreio/features/home/view/components/HomeTrackingCardList.kt
@@ -1,10 +1,14 @@
 package br.com.davidcastro.meurastreio.features.home.view.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import br.com.davidcastro.meurastreio.commons.components.TrackingCard
 import br.com.davidcastro.meurastreio.commons.utils.Dimens.dimen16dp
@@ -17,28 +21,37 @@ fun HomeTrackingCardList(
     list: List<TrackingDomain>,
     onItemClick: (TrackingDomain) -> Unit
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        items(list) { tracking ->
+    if(list.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Nenhum rastreio adicionado!")
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(list) { tracking ->
 
-            val lastEventStatus = tracking.getLastEvent()?.status.or {
-                "Nenhuma atualização no momento"
-            }
+                val lastEventStatus = tracking.getLastEvent()?.status.or {
+                    "Nenhuma atualização no momento"
+                }
 
-            TrackingCard(
-                modifier = Modifier.padding(bottom = dimen16dp),
-                name = tracking.name,
-                code = tracking.code,
-                status = lastEventStatus,
-                hasUpdate = tracking.hasUpdated.orFalse(),
-                date = tracking.getLastEvent()?.date.orEmpty(),
-                local = if(tracking.getLastEvent()?.subStatus?.isEmpty() == true)
-                    listOf(tracking.getLastEvent()?.local.orEmpty())
-                else
-                    tracking.getLastEvent()?.subStatus.orEmpty()
-            ) {
-                onItemClick(tracking)
+                TrackingCard(
+                    modifier = Modifier.padding(bottom = dimen16dp),
+                    name = tracking.name,
+                    code = tracking.code,
+                    status = lastEventStatus,
+                    hasUpdate = tracking.hasUpdated.orFalse(),
+                    date = tracking.getLastEvent()?.date.orEmpty(),
+                    local = if(tracking.getLastEvent()?.subStatus?.isEmpty() == true)
+                        listOf(tracking.getLastEvent()?.local.orEmpty())
+                    else
+                        tracking.getLastEvent()?.subStatus.orEmpty()
+                ) {
+                    onItemClick(tracking)
+                }
             }
         }
     }
